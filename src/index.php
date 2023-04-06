424a82d679bf91402276896c5522091d17253777
Kumar gaurav <GK838610@GMAIL.COM>
2023-04-07 00:23:39 +0530

    first commit

---
+++ src/index.php
@@ -0,0 +1,41 @@
+<?php
+	require "db_connect.php";
+	require "header.php";
+	session_start();
+	
+	if(empty($_SESSION['type']));
+	else if(strcmp($_SESSION['type'], "librarian") == 0)
+		header("Location: librarian/home.php");
+	else if(strcmp($_SESSION['type'], "member") == 0)
+		header("Location: member/home.php");
+?>
+
+<html>
+	<head>
+		<title>Book Buddy</title>
+		<link rel="stylesheet" type="text/css" href="css/index_style.css" />
+	</head>
+	<body>
+	 
+           
+           
+          </form></div>
+		<div id="allTheThings">
+			<div id="member">
+				<a href="member">
+					<img src="img/ic_member.svg" width="250px" height="auto"/><br />
+					&nbsp;Member Login
+				</a>
+			</div>
+			<div id="verticalLine">
+				<div id="librarian">
+					<a id="librarian-link" href="librarian">
+						<img src="img/ic_librarian.svg" width="250px" height="auto" /><br />
+						&nbsp;&nbsp;&nbsp;Librarian Login
+					</a>
+				</div>
+			</div>
+		</div>
+		
+	</body>
+</html>
